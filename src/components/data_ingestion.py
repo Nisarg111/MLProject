01cc0e872a7ad32ff17c4cdb9ff22ac57f139994
Nisarg111 <nisargvyas5555@gmail.com>
2024-07-10 18:13:15 +0530

    Artifact folder rename

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,9 +10,9 @@ from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path :str = os.path.join('artificats',"train.csv")
-    test_data_path :str = os.path.join('artificats',"test.csv")
-    raw_data_path :str = os.path.join('artificats',"raw.csv")
+    train_data_path :str = os.path.join('artifats',"train.csv")
+    test_data_path :str = os.path.join('artifacts',"test.csv")
+    raw_data_path :str = os.path.join('artifacts',"raw.csv")
 
 class DataIngestion:
     def __init__(self):
